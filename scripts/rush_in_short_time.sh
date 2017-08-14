e042d4213aaab44bbf94c4a24be8a1fbf6e7be7c
Heart-travel <zcgacbc@gmail.com>
2017-08-14 17:01:26 +0800

    Update rush_in_short_time.sh
    1. Don't generate the blank file
    2. Correct the wrong path for getting the source data

--- scripts/rush_in_short_time.sh
+++ scripts/rush_in_short_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT=/home/pi/source/stock
+ROOT=~/source/stock
 DATA=${ROOT}/data
 RESULT=${ROOT}/result
 SCRIPTS=${ROOT}/scripts
@@ -8,14 +8,20 @@ STOCK_FILE=${ROOT}/source/stock.txt
 
 for stock in `cat ${STOCK_FILE}`
 do
+	echo ${DATA}/${stock}/${stock}_rush.txt
 	:> ${DATA}/${stock}/${stock}_rush_in_7_days.txt
+
+	if [ ! -f "${DATA}/${stock}/${stock}_rush.txt" ]; then
+		rm ${DATA}/${stock}/${stock}_rush_in_7_days.txt
+		continue
+	fi
 
 	while read line
 	do
 		DATE=`echo ${line} | awk '{print $1}'`
 		LINENR=`grep -n ${DATE} ${DATA}/${stock}/${stock}_orig.txt | awk -F ":" '{print $1}'`
 
-		NXINFO=`sed -n "/${DATE}/{n;p;}" ${stock}/${stock}_rush.txt`
+		NXINFO=`sed -n "/${DATE}/{n;p;}" ${DATA}/${stock}/${stock}_rush.txt`
 		NXDATE=`echo ${NXINFO} | awk '{print $1}'`
 		NXLINENR=`grep -n ${NXDATE} ${DATA}/${stock}/${stock}_orig.txt | awk -F ":" '{print $1}'`
 
