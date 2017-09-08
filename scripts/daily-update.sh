901014f3ed8b364b824c531cb3b967f79d8b6db0
Heart-travel <zcgacbc@gmail.com>
2017-09-08 10:37:56 +0800

    20170905

--- scripts/daily-update.sh
+++ scripts/daily-update.sh
@@ -27,7 +27,7 @@ update_data_today(){
 	do
 		cd ${DATA}
 		DATE_T=`date +%Y%m%d`
-		DATE=`expr ${DATE_T} - 4`
+		DATE=`expr ${DATE_T} - 3`
 		curl http://quotes.money.163.com/service/chddata.html?code=${line}\&start=${DATE}\&end=${DATE} | tee ${DATA}/${line}/${line}_d_temp.txt
 		tac ${DATA}/${line}/${line}_d_temp.txt | tee ${line}/${line}_euc.txt
 	
